4eea93d7152ee4d2cd422b7f8fcb1a389571ccf0
Piotrek Cieślik <piotrekze@hotmail.com>
2021-01-23 00:24:47 +0100

    1

--- InzService.cs
+++ InzService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Threading;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
@@ -71,11 +72,14 @@ namespace InzService
             eventLog1.Source = "InzServiceSource";
             eventLog1.Log = "InzServiceLog";
 
-
             // => MQTT
             this.InitBroker();
             // <MQTT
         }
+
+        /// <summary>
+        /// Inicjalizacja brokera MQTT
+        /// </summary>
         private async void InitBroker()
         {
             MqttConfiguration config = new MqttConfiguration();
@@ -85,23 +89,13 @@ namespace InzService
 
             try
             {
-                sw.Start();
+                sw.Start(); // uruchomienie zegara
                 client = await MqttClient.CreateAsync(mqttBroker1, config);
-                //client2 = await MqttClient.CreateAsync(mqttBroker2, config2);
-
-
-                ss1 = await client.ConnectAsync(new MqttClientCredentials(clientId: this.clientId.Replace("-", String.Empty)));
-                //ss2 = await client.ConnectAsync(new MqttClientCredentials(clientId: this.clientId.Replace("-", String.Empty)));
-
-                await client.SubscribeAsync("pcipcipci", MqttQualityOfService.AtMostOnce);
-                //await client2.SubscribeAsync("pcipcipci", MqttQualityOfService.AtMostOnce);
-
-
-                client.MessageStream.Subscribe(msg => returnFormMqtt(msg.Topic, msg.Payload));
-                //client2.MessageStream.Subscribe(msg => returnFormMqtt(msg.Topic, msg.Payload));
-                sw.Stop();
-                CreateLog("Init Broker client1", $"OK time {sw.ElapsedMilliseconds} ms.");
-
+                ss1 = await client.ConnectAsync(new MqttClientCredentials(clientId: this.clientId.Replace("-", String.Empty))); // połaczenie do brokera z indywidualnym identyfikatorem
+                await client.SubscribeAsync("pcipcipci", MqttQualityOfService.AtMostOnce);  // subskrybcja tematu "pcipcipci"
+                client.MessageStream.Subscribe(msg => returnFormMqtt(msg.Topic, msg.Payload)); // Akcja wskazująca obsługę przychodzącej wiadomości przez wpisaną metodę
+                sw.Stop();  //zatrzymanie zegara
+                CreateLog("Init Broker client1", $"OK time {sw.ElapsedMilliseconds} ms."); // insert do bazy danych
             }
             catch (Exception ex)
             {
@@ -109,28 +103,16 @@ namespace InzService
                 CreateLog("Init Broker1 Exception", $"{ex.Message} time {sw.ElapsedMilliseconds} [ms]");
             }
 
+            // powtórzenie czynności dla drugiego brokera(aletrnatywnego)
             try
             {
                 sw.Start();
-                // CreateLog("after sw.start", "");
                 MqttConfiguration config2 = new MqttConfiguration();
-                // CreateLog("after config2", "");
-                //client = await MqttClient.CreateAsync(mqttBroker1, config);
                 client2 = await MqttClient.CreateAsync(mqttBroker2, config2);
-                //CreateLog("after client2", "");
-
-                //ss1 = await client.ConnectAsync(new MqttClientCredentials(clientId: this.clientId.Replace("-", String.Empty)));
                 ss2 = await client2.ConnectAsync(new MqttClientCredentials(clientId: this.clientId.Replace("-", String.Empty)));
-                //CreateLog("after ss2", "");
-                //await client.SubscribeAsync("pcipcipci", MqttQualityOfService.AtMostOnce);
                 await client2.SubscribeAsync("pcipcipci", MqttQualityOfService.AtMostOnce);
-                //CreateLog("after lient2.SubscribeAsync", "");
-
-                //client.MessageStream.Subscribe(msg => returnFormMqtt(msg.Topic, msg.Payload));
                 client2.MessageStream.Subscribe(msg => returnFormMqtt(msg.Topic, msg.Payload));
-                //CreateLog("after  client2.MessageStream", "");
                 sw.Stop();
-                //CreateLog("after sw.Stop", "");
                 CreateLog("Init Broker client2", $"OK time {sw.ElapsedMilliseconds} ms.");
             }
             catch (Exception ex)
@@ -144,15 +126,17 @@ namespace InzService
             }
         }
 
+        /// <summary>
+        /// Metoda obsługująca wiadomość z brokera
+        /// </summary>
+        /// <param name="topic">Subskrybowant Topic</param>
+        /// <param name="payload">Wiadomość</param>
         private void returnFormMqtt(string topic, byte[] payload)
         {
             Stopwatch = new Stopwatch();
             Stopwatch.Start();
             var t = topic;
             var txt = Encoding.UTF8.GetString(payload == null ? new byte[1] { 0 } : payload);
-            //var txt1 = Encoding.UTF32.GetString(payload);
-            //var txt2 = Encoding.UTF7.GetString(payload);
-
             string callback = txt;
             int idx = 0;
 
@@ -190,7 +174,7 @@ namespace InzService
                         var ms = Stopwatch.ElapsedMilliseconds;
                         Logs logs = new Logs();
                         logs.Title = $"Instert id={tt.Id} OK";
-                        logs.Date = DateTime.Now.ToString();
+                        logs.Date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
                         logs.RefTempId = tt.Id;
                         logs.Description = $"Operation time = {ms} [ms]";
                         db.Logs1.Add(logs);
@@ -218,50 +202,60 @@ namespace InzService
 
         protected override void OnStart(string[] args)
         {
-           
+
             // Update the service state to Start Pending.
-            ServiceStatus serviceStatus = new ServiceStatus(); 
+            ServiceStatus serviceStatus = new ServiceStatus();
             serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
             serviceStatus.dwWaitHint = 100000;
-            SetServiceStatus(this.ServiceHandle, ref serviceStatus); 
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
 
             eventLog1.WriteEntry("In OnStart.");
             // Set up a timer that triggers every minute.
-            Timer timer = new Timer();
+            System.Timers.Timer timer = new System.Timers.Timer();
             timer.Interval = 60000; // 60 seconds            
             timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
-            timer.Start(); 
+            timer.Start();
 
             // Update the service state to Running.
-            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING; 
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
             SetServiceStatus(this.ServiceHandle, ref serviceStatus);
 
-            this.DeleteOldRecords();
+            System.Threading.Thread thd = new System.Threading.Thread(this.DeleteOldRecords);
+            thd.Start();
         }
 
         private void DeleteOldRecords()
         {
             using (InzDataBase db = new InzDataBase())
             {
-                var sevedDaysAgo = DateTime.Now.AddDays(-7);
-                if (db.TemperatureTables.Any(x => x.Date < sevedDaysAgo))
-                {
-                    var query = db.TemperatureTables.Where(x => x.Date < sevedDaysAgo).ToList();
 
+                var sevenDaysAgo = DateTime.Now.AddDays(-7);
+                if (db.TemperatureTables.Any(x => x.Date < sevenDaysAgo))
+                {
+                    var query = db.TemperatureTables.Where(x => x.Date < sevenDaysAgo).ToList();
                     foreach (var item in query)
                     {
+                        var temp = db.TemperatureTables.Where(x => x.Id == item.Id).FirstOrDefault();
+                        if (temp != null)
+                        {
+                            db.TemperatureTables.Remove(temp);
+                            int rows = -1;
+                            try
+                            {
+                                rows = db.SaveChanges();
+                            }
+                            catch (Exception ex)
+                            {
+                                CreateLog("DeleteError", ex.Message.ToString());
+                            }
 
-                        db.Logs1.Remove(db.Logs1.Where(x => x.RefTempId == item.Id).FirstOrDefault());
-                        CreateLog("Delete successed !", $"for Id= {item.Id}");
-                        db.SaveChanges();
+                            if (rows > 0)
+                                CreateLog("Delete successed !", $"for Id= {item.Id}");
+                        }
                     }
-
-                    var lists = db.TemperatureTables.Where(x => x.Date < sevedDaysAgo).ToList();
-                    db.TemperatureTables.RemoveRange(lists);
-                    db.SaveChanges();
-
                 }
             }
+           
         }
 
         protected override void OnStop()
@@ -275,7 +269,8 @@ namespace InzService
 
             if (DateTime.Now > StartDate)
             {
-                DeleteOldRecords();
+                System.Threading.Thread thd = new System.Threading.Thread(this.DeleteOldRecords);
+                thd.Start();
             }
         }
 
@@ -286,6 +281,7 @@ namespace InzService
                 Logs logs = new Logs();
                 logs.Title = title;
                 logs.Description = $"Message: {desc}\t";
+                logs.Date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
                 // logs.RefTempId = "";
                 db.Logs1.Add(logs);
                 db.SaveChanges();
